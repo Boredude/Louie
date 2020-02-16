51d7c5f9c3c99f91d39393a80a9e13fdcf78cc46
omribitan <omri.bitan@guesty.com>
2020-02-16 23:09:09 +0200

    add support for tabs

--- core/UI.js
+++ core/UI.js
@@ -87,7 +87,7 @@ function listenToChatPanelChanges() {
 						   mutation.target.parentNode.parentNode.nodeName.toLowerCase() === "span" &&
 						   mutation.target.parentNode.parentNode.classList.contains("_3NWy8")) {
 					// TODO: find the name and add logo to it
-					console.log(mutation.target.data);
+					//console.log(mutation.target.data);
 				}
 			}
 		});
@@ -136,77 +136,99 @@ function addIconIfNeeded()
 {
 	if (document.getElementsByClassName("menu-item-incognito").length > 0) return; // already added
 	
-	var firstMenuItem = document.getElementsByClassName("_3j8Pd")[0];
-	if (firstMenuItem != undefined)
+	// fetch contacts
+	chrome.runtime.sendMessage({ name: "getContacts" }, function (contacts) 
 	{
-		var menuItemElem = document.createElement("div");
-		menuItemElem.setAttribute("id", "louis-dropdown");
-		menuItemElem.setAttribute("class", "_3j8Pd menu-item-incognito");
-		
-		var iconElem = document.createElement("button");
-		iconElem.setAttribute("class", "icon icon-incognito");
-		iconElem.setAttribute("title", "Stay in touch");
-		
-		// if we have urgencies 
-		if (window.urgencies && 
-			window.urgencies.count > 0) {
-			// add notification icon
-			var notificationElem = document.createElement("span");
-			notificationElem.setAttribute("class", "louis-notification");
-			// set number of pending urgencies
-			notificationElem.textContent = window.urgencies.count;
-			// append to icon element
-			iconElem.appendChild(notificationElem);
-		}
-
-		menuItemElem.appendChild(iconElem);
-		firstMenuItem.parentElement.insertBefore(menuItemElem, firstMenuItem);
-		
-		const dropContent = buildStayInTouchDropdownContent(window.urgencies);
-					
-		var drop = new Drop(
+		var firstMenuItem = document.getElementsByClassName("_3j8Pd")[0];
+		if (firstMenuItem != undefined)
 		{
-			target: menuItemElem,
-			content: dropContent,
-			position: "bottom left",
-			classes: "dropdown-theme",
-			openOn: "click",
-			tetherOptions: 
+			var menuItemElem = document.createElement("div");
+			menuItemElem.setAttribute("id", "louis-dropdown");
+			menuItemElem.setAttribute("class", "_3j8Pd menu-item-incognito");
+			
+			var iconElem = document.createElement("button");
+			iconElem.setAttribute("class", "icon icon-incognito");
+			iconElem.setAttribute("title", "Stay in touch");
+			
+			// if we have urgencies 
+			if (window.urgencies && 
+				window.urgencies.count > 0) {
+				// add notification icon
+				var notificationElem = document.createElement("span");
+				notificationElem.setAttribute("class", "louis-notification");
+				// set number of pending urgencies
+				notificationElem.textContent = window.urgencies.count;
+				// append to icon element
+				iconElem.appendChild(notificationElem);
+			}
+
+			menuItemElem.appendChild(iconElem);
+			firstMenuItem.parentElement.insertBefore(menuItemElem, firstMenuItem);
+			
+			const dropContent = buildStayInTouchDropdownContent(window.urgencies, contacts);
+			
+			const onNotificationTabClick = (e) => {
+				document.getElementById("c1").setAttribute("class", "content");
+				document.getElementById("c2").setAttribute("class", "content hidden");
+			};
+
+			const onContactsTabClick = (e) => {
+				document.getElementById("c1").setAttribute("class", "content hidden");
+				document.getElementById("c2").setAttribute("class", "content");
+			};
+
+			const onContactClick = (e) => {
+				// get id
+				const jid = e.currentTarget.id;
+				// close drop
+				drop.close();
+				// dispatch event
+				setTimeout(() => {
+					// open stay in touch dialog
+					document.dispatchEvent(new CustomEvent('onOpenStayInTouchDialog', {detail: JSON.stringify(contacts[jid])}));	
+				}, 100);
+			}
+			
+			var drop = new Drop(
 			{
-				offset: "-4px -4px 0 0"
-			},
-		});
-		var originalCloseFunction = drop.close;
-		drop.close = function()
-		{
-			//document.dispatchEvent(new CustomEvent('onIncognitoOptionsClosed', {detail: null}));
-			setTimeout(function() {originalCloseFunction.apply(drop, arguments); }, 100);
+				target: menuItemElem,
+				content: dropContent,
+				position: "bottom left",
+				classes: "dropdown-theme",
+				openOn: "click",
+				tetherOptions: 
+				{
+					offset: "-4px -4px 0 0"
+				},
+			});
+			var originalCloseFunction = drop.close;
+			drop.close = function()
+			{
+				//document.dispatchEvent(new CustomEvent('onIncognitoOptionsClosed', {detail: null}));
+				setTimeout(function() {originalCloseFunction.apply(drop, arguments); }, 100);
+			}
+			drop.on("open", function()
+			{
+				if (!checkInterception()) return;
+				
+				document.getElementById("tab1").addEventListener("click", onNotificationTabClick);
+				document.getElementById("tab2").addEventListener("click", onContactsTabClick);
+				
+				for (let e of document.getElementsByClassName("contact")) {
+					e.addEventListener("click", onContactClick)
+				}
+			});
+			drop.on("close", function()
+			{
+				document.getElementById("tab1").removeEventListener("click", onNotificationTabClick);
+				document.getElementById("tab2").removeEventListener("click", onContactsTabClick);
+
+				for (let e of document.getElementsByClassName("contact")) {
+					e.removeEventListener("click", onContactClick)
+				}
+			});
 		}
-		drop.on("open", function()
-		{
-			if (!checkInterception()) return;
-			
-			// document.getElementsByClassName("menu-item-incognito")[0].setAttribute("class", "_3j8Pd GPmgf active menu-item-incognito");
-
-			// document.getElementById("incognito-option-read-confirmations").addEventListener("click", onReadConfirmaionsTick);
-			// document.getElementById("incognito-option-presence-updates").addEventListener("click", onPresenseUpdatesTick);
-			// document.getElementById("incognito-option-safety-delay").addEventListener("input", onSafetyDelayChanged);
-			// document.getElementById("incognito-option-safety-delay").addEventListener("keypress", isNumberKey);
-			// document.getElementById("incognito-radio-enable-safety-delay").addEventListener("click", onSafetyDelayEnabled);
-			// document.getElementById("incognito-radio-disable-safety-delay").addEventListener("click", onSafetyDelayDisabled);
-			
-			// document.dispatchEvent(new CustomEvent('onIncognitoOptionsOpened', {detail: null}));
-		});
-		drop.on("close", function()
-		{
-			// document.getElementsByClassName("menu-item-incognito")[0].setAttribute("class", "_3j8Pd menu-item-incognito");
-
-			// document.getElementById("incognito-option-read-confirmations").removeEventListener("click", onReadConfirmaionsTick);
-			// document.getElementById("incognito-option-presence-updates").removeEventListener("click", onPresenseUpdatesTick);
-			// document.getElementById("incognito-radio-enable-safety-delay").removeEventListener("click", onSafetyDelayEnabled);
-			// document.getElementById("incognito-radio-disable-safety-delay").removeEventListener("click", onSafetyDelayDisabled);
-		});
-	}
+	});
 }
 
 
@@ -352,6 +374,7 @@ document.addEventListener('onOpenStayInTouchDialog', function(e)
 	});
 });
 
+
 /*
  *	Calculate the urgency to stay in touch with the contact
  *
@@ -382,7 +405,7 @@ function calculateStayInTouchUrgency(lastTimestmap, nowTimestamp, schedule) {
 	if (difference > 0) {
 		// if more than 10 days have passed from the moment we needed to chat with contact, it's defined urgent
 		// if 1 to 9 days have passed from the moment we needed to chat with contact, it's defined high
-		return difference >= 10 ? "urgent" : "hight";
+		return difference >= 10 ? "urgent" : "high";
 	// if difference is negative meaning we haven't passed the time we defined
 	} else {
 		// if less than one day left until the moment we needed to chat with contact, it's defined medium
@@ -390,38 +413,13 @@ function calculateStayInTouchUrgency(lastTimestmap, nowTimestamp, schedule) {
 		// otherwise not urgent at all
 		return difference >= -1 
 					 ? "medium" 
-					 : difference >= 5 ? "low"
+					 : difference >= -5 ? "low"
 					 : "none";
 	}
 }
 
 
-function buildStayInTouchDropdownContent(urgencies) {
-	
-	let notificationContent = '<div class="louis-dropdown-container">';
-
-	if (urgencies && urgencies.count > 0) {
-		notificationContent += '<ul class="louis-dropdown-list">';
-
-		// iterate through the urgencies
-		for (urgency in urgencies) {
-			// skip the property count
-			if (urgency === "count") continue;
-			// foreach contacts in the urgency
-			for (contact of urgencies[urgency]) {
-				// add li to content
-				notificationContent += `<li class="louis-dropdown-item _3zy-4 Sl-9e urgency_${urgency}">${contact.formattedName}</li>`;
-			}
-		}
-		
-		notificationContent += '</ul>';
-	} else {
-		notificationContent += '<div id="all-caught-up">You are all caught up! :)</div>'
-	}
-
-	notificationContent += '</div>';
-
-	const contactsContent = "<h1>Hello</h1>";
+function buildStayInTouchDropdownContent(urgencies, contacts) {
 
 	return `
 	<div class="container">
@@ -432,16 +430,97 @@ function buildStayInTouchDropdownContent(urgencies) {
 		<div class="line"></div>
 		<div class="content-container">
 			<div class="content" id="c1">
-			<h3>Notifications</h3>
-				${notificationContent}
+				${buildStayInTouchDropdownNotificationContent(urgencies)}
 			</div>
-			<div class="content" id="c2">
-			<h3>Contacts</h3>
-				${contactsContent}
+			<div class="content hidden" id="c2">
+				${buildStayInTouchDropdownContactsContent(contacts)}
 			</div>
 		</div> 
 	</div>
 	`;
+}
+
+function buildStayInTouchDropdownNotificationContent(urgencies) {
+	
+	let content = '<div class="louis-dropdown-container">';
+
+	if (urgencies && urgencies.count > 0) {
+		
+		content += '<ul class="louis-dropdown-list">';
+
+		if (urgencies.hasOwnProperty("urgent")) {
+			content += buildNotificationItem(urgencies["urgent"], "urgent");
+		} 
+		if (urgencies.hasOwnProperty("high")) {
+			content += buildNotificationItem(urgencies["high"], "high");
+		} 
+		if (urgencies.hasOwnProperty("medium")) {
+			content += buildNotificationItem(urgencies["medium"], "medium");
+		} 
+		if (urgencies.hasOwnProperty("low")) {
+			content += buildNotificationItem(urgencies["low"], "low");
+		} 
+		
+		content += '</ul>';
+	} else {
+		content += '<h3 id="all-caught-up">You are all caught up!</h3>'
+	}
+
+	content += '</div>';
+
+	return content;
+}
+
+function buildNotificationItem(contacts, urgency) {
+
+	let content = "";
+	// foreach contacts in the urgency
+	for (contact of contacts) {
+		// add li to content
+		content += `<li class="louis-dropdown-item _3zy-4 Sl-9e urgency">
+						<h3>${contact.formattedName}</h3>
+						<span class="urgency_${urgency}">${urgency}</span>
+					</li>`;
+	}
+
+	return content;
+} 
+
+function buildStayInTouchDropdownContactsContent(contacts) {
+	
+	let content = '<div class="louis-dropdown-container">';
+	// if we have contacts defined
+	if (contacts && Object.entries(contacts).length > 0) {
+		content += '<ul class="louis-dropdown-list">';
+
+		// iterate through the contacts
+		for (jid in contacts) {
+			// add li to content
+			const contact = contacts[jid];
+			let quantity, frequency;
+			if (contact.schedule.quantity === "1") {
+				quantity = "once";
+				frequency = `a ${contact.schedule.frequency}`;
+			} else if (contact.schedule.quantity === "2") {
+				quantity = "twice";
+				frequency = `a ${contact.schedule.frequency}`;
+			} else {
+				quantity = contact.schedule.quantity;
+				frequency = `times a ${contact.schedule.frequency}`;
+			}
+			content += `<li class="louis-dropdown-item _3zy-4 Sl-9e contact" id="${jid}">
+							<h3>${contact.formattedName} ${quantity} ${frequency}</h3>
+						</li>`;
+		}
+		
+		content += '</ul>';
+	} else {
+		content += '<h3 id="all-caught-up">So who would you like to stay in touch with?</h3>'
+	}
+
+	content += '</div>';
+
+	return content;
 }
 
 function buildStayInTouchDialogContent(data, contacts) {
